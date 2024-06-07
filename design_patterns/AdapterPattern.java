b5149d9fb72c8f6b5b863f3918d9203c0ff8a640
prajjwalkumar17 <talk2prajjwal@gmail.com>
2024-06-07 23:09:46 +0530

    chore: initial commit with builder and Adapter patterns

---
+++ design_patterns/AdapterPattern.java
@@ -0,0 +1,48 @@
+package design_patterns;
+
+class College {
+  public static void main(String[] args) {
+    // You got this from your friend
+    PilotPen pp = new PilotPen();
+    // You got a pen from the PilotPen which will help you to create your assignment
+    Pen p = new PenAdapter(pp);
+    Assignment a = new Assignment(p);
+    a.write_assignment("Assignment for computer organization");
+  }
+}
+
+
+// Required to write an assignment
+interface Pen {
+  public String write();
+}
+
+// You borrowed this from your friend as you/he didn't had pen
+class PilotPen {
+}
+
+// Converts the PilotPen into the Pen required to write assignment
+class PenAdapter implements Pen {
+  PilotPen p;
+  public PenAdapter(PilotPen p) {
+    this.p = p;
+  }
+@Override
+public String write() {
+	return "writing";
+}
+}
+
+// The work given by your college which requires a pen to be completed
+class Assignment {
+  Pen p;
+
+  public Assignment(Pen p) {
+    this.p = p;
+  }
+
+public void write_assignment(String assignment_content) {
+    System.out.println("Assignment is being written from pen and the content is: "+assignment_content);
+  }
+}
+
---
+++ design_patterns/BuilderPattern.java
@@ -0,0 +1,32 @@
+package design_patterns;
+
+class phone {
+  public static void main(String[] args) {
+    PhoneBuilder ph = new PhoneBuilder().setOs("android").setScreenSize(21).getPhone();
+  }
+}
+
+class PhoneBuilder {
+  public String Os;
+  public int ScreenSize;
+  public Float Camera;
+
+  public PhoneBuilder() {
+  }
+  public PhoneBuilder setOs(String os) {
+      Os = os;
+      return this;
+  }
+  public PhoneBuilder setCamera(Float camera) {
+      Camera = camera;
+      return this;
+  }
+  public PhoneBuilder setScreenSize(int sc) {
+      ScreenSize  = sc;
+      return this;
+  }
+  public PhoneBuilder getPhone(){
+    System.out.println("The phone is:\n" +Os +"\n"+ScreenSize+"\n"+Camera);
+      return this;
+  }
+}
